35c301f5bcfbc592f8ffa18279534de762bc56c3
ZhengyuZhao <z.zhao@cs.ru.nl>
2019-04-07 19:05:57 +0200

    Update prepare.sh

--- Archive-MM-RP/utils/prepare.sh
+++ Archive-MM-RP/utils/prepare.sh
@@ -1,12 +1,8 @@
 mkdir datasets
 mkdir results
 
-mkdir datasets/SUN397
-mkdir datasets/Places
-mkdir datasets/SUN397/images
-mkdir datasets/SUN397/labels
-mkdir datasets/Places/images
-mkdir datasets/Places/labels
+mkdir datasets/images
+mkdir datasets/labels
 mkdir results/intermediate
 mkdir results/intermediate/Places
 mkdir results/intermediate/SUN397
@@ -17,17 +13,20 @@ wget http://vision.princeton.edu/projects/2010/SUN/SUN397.tar.gz
 wget http://data.csail.mit.edu/places/places365/val_large.tar
 
 echo "Decompressing datasets..."
-tar -C datasets/SUN397/images/ -xvf SUN397.tar.gz
-tar -C datasets/Places/images/ -xvf val_large.tar
+tar -C datasets/images/ -xvf SUN397.tar.gz
+tar -C datasets/images/ -xvf val_large.tar
+mv -T datasets/images/val_large datasets/images/Places
 
 echo "Deleting compressed data..."
-rm ./SUN397.tar.gz
-rm ./val_large.tar
+rm SUN397.tar.gz
+rm val_large.tar
  
+mkdir datasets/labels/SUN397/
+mkdir datasets/labels/Places/
 
 echo "Downloading labels..."
 wget https://vision.princeton.edu/projects/2010/SUN/download/Partitions.zip
-unzip Partitions.zip -d datasets/SUN397/labels/
+unzip Partitions.zip -d datasets/labels/SUN397/
 
 wget http://data.csail.mit.edu/places/places365/filelist_places365-standard.tar
-tar -C datasets/Places/labels/ -xvf filelist_places365-standard.tar
+tar -C datasets/labels/Places/ -xvf filelist_places365-standard.tar
