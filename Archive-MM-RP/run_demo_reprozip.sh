3006217a9416e8437faaa2de2ee74efe4c4560b9
ZhengyuZhao <z.zhao@cs.ru.nl>
2019-04-08 15:12:03 +0200

    change args for svm_classification

--- Archive-MM-RP/run_demo_reprozip.sh
+++ Archive-MM-RP/run_demo_reprozip.sh
@@ -8,7 +8,7 @@ python ./utils/dis_map_generation.py -batch_size 256 -datasets 'Places' -gpu 1
 
 python ./utils/adaptive_region_selection.py -datasets 'Places' -window_params 3 1
 
-python ./utils/intra_scale_feature_extraction.py -datasets 'Places' -arches 'alexnet' -selection_types 'adi_red' -thresholds 100 150 -resolution 'ori_res' -scales 1 2 3
+python ./utils/intra_scale_feature_extraction.py -datasets 'Places' -arches 'alexnet' -selection_types 'adi_red' -resolution 'ori_res' -scales 1 2 3
 
 python ./utils/svm_classification.py -datasets 'Places' -arches 'alexnet' -scales 1 2 3 -T2 150 -T3 100 -selection_types 'adi_red' -thresholds 100 150 -resolution 'ori_res' 
 
